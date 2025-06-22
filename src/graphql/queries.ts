2478422994e58898aaf6ffcd5eb8b4dd4e09b5e2
Lucas Paulo <lucaspaulodev@gmail.com>
2025-06-22 17:35:42 -0300

    Initial commit: project scaffolding, configs, and base source structure

---
+++ src/graphql/queries.ts
@@ -0,0 +1,70 @@
+import { gql } from '@apollo/client';
+
+export const GET_POSTS = gql`
+  query GetPosts($first: Int, $after: String, $order: PostsOrder, $postedBefore: DateTime, $postedAfter: DateTime) {
+    posts(first: $first, after: $after, order: $order, postedBefore: $postedBefore, postedAfter: $postedAfter) {
+      edges {
+        cursor
+        node {
+          id
+          name
+          tagline
+          thumbnail {
+            url
+          }
+          votesCount
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`;
+
+export const GET_POST_DETAILS = gql`
+  query GetPostDetails($id: ID!) {
+    post(id: $id) {
+      id
+      name
+      tagline
+      description
+      commentsCount
+      topics {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      makers {
+        name
+        profileImage
+      }
+    }
+  }
+`;
+
+export const SEARCH_POSTS = gql`
+  query SearchPosts($query: String!, $first: Int, $after: String, $postedBefore: DateTime, $postedAfter: DateTime) {
+    posts(search: $query, first: $first, after: $after, postedBefore: $postedBefore, postedAfter: $postedAfter) {
+      edges {
+        cursor
+        node {
+          id
+          name
+          tagline
+          thumbnail {
+            url
+          }
+          votesCount
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`;
